e464279a3c394497ff5ebf7bcada4fbb2b0f92f7
NivekNK <juegosdelhambre25@outlook.com>
2021-06-24 20:12:18 -0400

    Update

--- mUsers.c
+++ mUsers.c
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
@@ -110,78 +109,4 @@ void printUser(User* user)
     printf("Nombre: %s\n", user->name);
     printf("Apellido: %s\n", user->surname);
     printf("RUT: %s\n", user->rut);
-=======
-#include <stdio.h>
-#include <stdbool.h>
-#include <string.h>
-#include <list.h>
-#include <Map.h>
-#include <mUsers.h>
-
-struct User
-{
-    char* name;
-    char* surname;
-    char* rut;
-    bool admin;
-    List* discounts;
-};
-
-typedef User User;
-
-User* createUser(char* name, char* surname, char* rut, bool admin, List* discounts)
-{
-    User* user = (User*)malloc(sizeof(User));
-    strcpy(user->name, name);
-    strcpy(user->surname, surname);
-    strcpy(user->rut, rut);
-    user->admin = admin;
-    user->discounts = discounts;
-}
-
-void registerUser(Map* users)
-{    
-    char* name = (char*)malloc(sizeof(char*) * 20);
-    char* surname = (char*)malloc(sizeof(char*) * 20);
-    char* rut =(char*)malloc(sizeof(char*) * 20);
-    char* esAdmin = (char*)malloc(sizeof(char*) * 3);
-    bool admin = false;
-    List* discounts = createList();
-    
-    printf("Ingrese Nombre\n");
-    scanf("%s", name);
-    printf("Ingrese Apellido\n");
-    scanf("%s", surname);
-    printf("Ingrese RUT\n");
-    scanf("%s", rut);
-    printf("¿El nuevo usuario es administrador?, ingrese SI o NO");
-    scanf("%s", esAdmin);
-    // falta ciclo para comprobar esAdmin //
-    if ( strcmp("SI", esAdmin) == 0 ) admin = true;
-    if ( stcmp("NO", esAdmin) == 0 ) admin = false;
-    
-    User* user = createUser(name, surname, rut, admin, discounts);
-    insertMap(users, rut, user);
-    
-}
-
-User* logIn(Map* users)
-{
-    char* rut =(char*)malloc(sizeof(char*) * 20);
-    printf("Ingrese su RUT\n");
-    scanf("%s", rut);
-
-    User* user = searchMap(users, rut);
-    return user;
-}
-
-bool isAdmin(User* user)
-{
-    return user->admin;
-}
-
-char* getRut(User* user)
-{
-    return user->rut;
->>>>>>> c76b294740a02ae13ffc2915946a5a27413b500c
 }
--- mUsers.h
+++ mUsers.h
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include <stdio.h>
 #include <stdbool.h>
 #include "Map.h"
@@ -23,18 +22,3 @@ char* getRut(User* user);
 void printUser(User* user);
 
 #endif /* M_Users */
-=======
-#include <stdio.h>
-#include <Map.h>
-#include <stdbool.h>
-
-typedef struct User User;
-
-void registerUser(Map* users);
-
-User* logIn(Map* users);
-
-bool isAdmin(User* user);
-
-char* getRut(User* user);
->>>>>>> c76b294740a02ae13ffc2915946a5a27413b500c
