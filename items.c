e6a063b2ad5b92e49699607af085d15b5ac96933
Pipasoo <felipe.godoy.t@mail.pucv.cl>
2021-07-05 18:17:30 -0400

    Items

--- items.c
+++ items.c
@@ -1,10 +1,8 @@
 #include <stdbool.h>
-#include <string.h>
 #include "items.h"
 #include "list.h"
-#include "Map.h"
 
-struct Item
+struct item
 {
     int id;
     int price;
@@ -14,7 +12,7 @@ struct Item
     char *type;
 };
 
-struct Category
+struct category
 {
     List* items;
     char *type;
@@ -22,100 +20,16 @@ struct Category
     int current;
 };
 
-struct Discounts
+struct discounts
 {
     int discount;
     bool isItem;
-    Item* item;
+    item* item;
     char *type;
 };
 
-typedef Item Item;
-typedef Category Category;
-typedef Discounts Discounts;
+typedef item item;
+typedef category category;
+typedef discounts discounts;
 
-Category* createEmptyCategory()
-{
-    Category* category = (Category*)malloc(sizeof(Category));
 
-    category->items = createList();
-    category->type = (char*)malloc(sizeof(char) * 20);
-    category->max = 0;
-    category->current = 0;
-
-    return category;
-}
-
-void createCategory()
-{
-    
-}
-
-Item* createEmptyItem()
-{
-    Item* item = (Item*)malloc(sizeof(Item));
-    
-    item->id = 0;
-    item->price = 0;
-    item->stock = 0;
-    item->name = (char*)malloc(sizeof(char) * 20);
-    item->brand = (char*)malloc(sizeof(char) * 20);
-    item->type = (char*)malloc(sizeof(char) * 20);
-    
-    return item;
-}
-
-void createItem(Map* inventory, Map* category)
-{
-    Item* item = createEmptyItem();
-    
-    int size = sizeMapa(inventory);
-    item->id = size;
-
-    printf("Ingrese el nombre del producto\n");
-    scanf("%s",item->name);
-    printf("Ingrese la marca del producto\n");
-    scanf("%s",item->brand);
-    printf("Ingrese el tipo de producto\n");
-    scanf("%s",item->type);
-    printf("Ingrese el precio del producto\n");
-    scanf("%d",&item->price);
-    printf("Ingrese la cantidad stock");
-    scanf("%d",&item->stock);
-    insertMap(inventory,&item->id,item);
-    
-    Category* aux = searchMap(category, item->type);
-    if (aux != NULL)
-    {
-        pushBack(aux->items, item);
-        aux->current += item->stock;
-    }
-    else
-    {
-        
-    }
-}
-
-void addItem(Map* inventory)
-{
-    //añadir comprobacion  añandir resto de mapas
-    int id;
-    int stock;
-    scanf("%i", &id);
-    scanf("%i", &stock);
-
-    Item* item = searchMap(inventory, &id);
-    item->stock=+stock;
-}
-
-void removeItemFromId(Map* inventory)
-{
-    //añadir comprobacion,quitar en resto de mapas
-    int id;
-    int stock;
-    scanf("%i", &id);
-    scanf("%i", &stock);
-
-    Item* item = searchMap(inventory, &id);
-    item->stock=-stock;
-}
