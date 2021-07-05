dc5dc6689ef90c58ba4c4a22654eaecca0ddf633
JavierFarina <javier.farina.a@mail.pucv.cl>
2021-07-05 19:21:00 -0400

    Funciones finales y arregos pequeños

--- mUsers.c
+++ mUsers.c
@@ -2,9 +2,11 @@
 #include <stdlib.h>
 #include <stdbool.h>
 #include <string.h>
+#include <ctype.h>
 #include "mUsers.h"
 #include "list.h"
 #include "Map.h"
+#include "items.h"
 
 struct User
 {
@@ -42,8 +44,40 @@ User* createUser(char* name, char* surname, char* rut, bool admin)
     return user;
 }
 
+void setUserValue(User* user, char* value, int i)
+{
+    switch (i)
+    {
+        case 0:
+            user->name = value;
+            break;
+        case 1:
+            user->surname = value;
+            break;
+        case 2:
+            user->rut = value;
+            break;
+        case 3:
+            if (strcmp("SI", value) == 0)
+            {
+                user->admin = true;
+            }
+            else if (strcmp("NO", value) == 0)
+            {
+                user->admin = false;
+            }
+            break;
+    }
+}
+
 void copyUser(User* copy, User* toCopy)
 {
+    if (toCopy == NULL)
+    {
+        printf("Usuario no encontrado!\n");
+        return;
+    }
+
     copy->rut = toCopy->rut;
     copy->name = toCopy->name;
     copy->surname = toCopy->surname;
@@ -51,16 +85,166 @@ void copyUser(User* copy, User* toCopy)
     copy->discounts = toCopy->discounts;
 }
 
+bool isRut(char* rut, char* check)
+{
+    if (strlen(rut) > 12)
+    {
+        return false;
+    }
+
+    if (isalpha(rut[11]))
+    {
+        rut[11] = toupper(rut[11]);
+    }
+
+    int j = 0;
+    for (int i = 0; i < 12; i++)
+    {
+        switch (i)
+        {
+            case 2:
+                if (rut[i] != '.')
+                {
+                    return false;
+                }
+                break;
+            case 6:
+                if (rut[i] != '.')
+                {
+                    return false;
+                }
+                break;
+            case 10:
+                if (rut[i] != '-')
+                {
+                    return false;
+                }
+                break;
+            case 11:
+                if (isalpha(rut[i]))
+                {
+                    if (rut[i] != 'K')
+                    {
+                        return false;
+                    }
+                }
+                else if (!isdigit(rut[i]))
+                {
+                    return false;
+                }
+                break;
+            default:
+                if (!isdigit(rut[i]))
+                {
+                    return false;
+                }
+                check[j] = rut[i];
+                j++;
+                break;
+        }
+    }
+
+    return true;
+}
+
+bool isRealRut(char* rut)
+{
+    int serie[6] = {2, 3, 4, 5, 6, 7};
+    char* check = (char*)malloc(sizeof(char) * 20);
+    char digit = rut[11];
+    int j = 0;
+    int value = 0;
+    
+    if (isRut(rut, check))
+    {
+        for (int i = 7; i >= 0; i--)
+        {
+            char *ptr = (char*)malloc(sizeof(char) * 2);
+            ptr[0] = check[i];
+            ptr[1] = '\0';
+            value += (atoi(ptr) * serie[j]);
+            free(ptr);
+            j++;
+            if (j > 5) j = 0;
+        }
+        free(check);
+
+        value = value % 11;
+        value = 11 - value;
+        if (value == 11)
+        {
+            value = 0;
+        }
+
+        if (value == 10)
+        {
+            if (isalpha(digit))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        else
+        {
+            if (isdigit(digit))
+            {
+                char *ptr = (char*)malloc(sizeof(char) * 2);
+                ptr[0] = digit;
+                ptr[1] = '\0';
+                if (value == atoi(ptr))
+                {
+                    free(ptr);
+                    return true;
+                }
+                else
+                {
+                    free(ptr);
+                    return false;
+                }
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+    else
+    {
+        free(check);
+        return false;
+    }
+}
+
+char* checkRut()
+{
+    char* rut = (char*)malloc(sizeof(char) * 20);
+    scanf("%s", rut);
+    while (!isRealRut(rut))
+    {
+        printf("RUT incorrecto, ingrese otro: ");
+        scanf("%s", rut);
+    }
+
+    if (isalpha(rut[11]))
+    {
+        rut[11] = toupper(rut[11]);
+    }
+
+    return rut;
+}
+
 void registerUser(Map* users)
-{    
-    char* rut =(char*)malloc(sizeof(char) * 20);
+{
     char* name = (char*)malloc(sizeof(char) * 20);
     char* surname = (char*)malloc(sizeof(char) * 20);
     char* esAdmin = (char*)malloc(sizeof(char) * 3);
     bool admin = false;
 
     printf("Ingrese RUT: ");
-    scanf("%s", rut);
+    char* rut = checkRut();
     printf("Ingrese Nombre: ");
     scanf("%s", name);
     printf("Ingrese Apellido: ");
@@ -79,13 +263,20 @@ void registerUser(Map* users)
 
 User* logIn(Map* users)
 {
-    char* rut = (char*)malloc(sizeof(char) * 20);
     printf("Ingrese su RUT: ");
-    scanf("%s", rut);
+    char* rut = checkRut();
 
     User* user = searchMap(users, rut);
     free(rut);
-    return user;
+    if (user) return user;
+    else return NULL;
+}
+
+void logOut(User* user)
+{
+    User* empty = createEmptyUser();
+    empty->rut = "";
+    copyUser(user, empty);
 }
 
 bool isAdmin(User* user)
@@ -114,4 +305,92 @@ void printUser(User* user)
     printf("Nombre: %s\n", user->name);
     printf("Apellido: %s\n", user->surname);
     printf("RUT: %s\n", user->rut);
+}
+
+void addDiscount(Map* users)
+{
+    printf("Ingrese RUT de usuario: ");
+    char* rut = checkRut();
+
+    User* user = searchMap(users, rut);
+    if (user == NULL)
+    {
+        printf("Usuario no encontrado!\n");
+        free(rut);
+        return;
+    }
+
+    free(rut);
+    Discounts* discount = createDiscount();
+    if (discount == NULL) return;
+    pushBack(user->discounts, discount);
+}
+
+void cleanDiscounts(Map* users)
+{
+    printf("Ingrese RUT de usuario: ");
+    char* rut = checkRut();
+
+    User* user = searchMap(users, rut);
+    if (user == NULL)
+    {
+        printf("Usuario no encontrado!\n");
+        free(rut);
+        return;
+    }
+
+    cleanList(user->discounts);
+    free(rut);
+}
+
+List* getDiscounts(User* user)
+{
+    return user->discounts;
+}
+
+void socialClub(User* user, Map* users, Map* itemPerId)
+{
+    if (isAdmin(user))
+    {
+        printf("Ingrese RUT de usuario a revisar: ");
+        char* rut = checkRut();
+        User* aux = searchMap(users, rut);
+        if (aux != NULL)
+        {
+            printDiscounts(aux, itemPerId);
+        }
+        else
+        {
+            printf("Usuario no encontrado!\n");
+        }
+
+        free(rut);
+    }
+    else
+    {
+        printDiscounts(user, itemPerId);
+    }
+}
+
+char* userToChar(User* user)
+{
+    char* string = (char*)malloc(sizeof(char) * 100);
+    strcpy(string, user->name);
+    strcat(string, ",");
+    strcat(string, user->surname);
+    strcat(string, ",");
+    strcat(string, user->rut);
+    strcat(string, ",");
+
+    if (user->admin)
+    {
+        strcat(string, "SI");
+    }
+    else
+    {
+        strcat(string, "NO");
+    }
+    strcat(string, ",\n");
+
+    return string;
 }
--- mUsers.h
+++ mUsers.h
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdbool.h>
 #include "Map.h"
+#include "list.h"
 
 #ifndef MUsers_h
 #define MUsers_h
@@ -9,11 +10,15 @@ typedef struct User User;
 
 User* createEmptyUser();
 
+void setUserValue(User* user, char* value, int i);
+
 void copyUser(User* copy, User* toCopy);
 
 void registerUser(Map* users);
 
 User* logIn(Map* users);
+
+void logOut(User* user);
 
 bool isAdmin(User* user);
 
@@ -22,5 +27,15 @@ char* getRut(User* user);
 void setRut(User* user, char* s);
 
 void printUser(User* user);
+
+void addDiscount(Map* users);
+
+void cleanDiscounts(Map* users);
+
+List* getDiscounts(User* user);
+
+void socialClub(User* user, Map* users, Map* itemPerId);
+
+char* userToChar(User* user);
 
 #endif /* M_Users */
